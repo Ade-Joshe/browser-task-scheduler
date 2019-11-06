661fc8b6470b4de0e3a7f82b503010560bde1fd9
valentine-mario <valentine13400@gmail.com>
2019-11-06 11:38:52 +0100

    set up the browser scheduler chrome extension

---
+++ js/background.js
@@ -0,0 +1,12 @@
+chrome.runtime.onInstalled.addListener(function() {
+    alert("welcome")
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: '*'},
+          })
+          ],
+              actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+      });
+  });
