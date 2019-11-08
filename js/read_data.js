3ab7160ddd7e1d90688b48eed8a3bfef4aa6d7a1
valentine-mario <valentine13400@gmail.com>
2019-11-08 14:39:03 +0100

    added open to view list by onclick action and fixed bug for posting data

--- js/read_data.js
+++ js/read_data.js
@@ -1,44 +1,37 @@
-
-        json=JSON.parse(localStorage.getItem('browser-schedule'))
-        var str = '<ul>'
-
-        json.forEach(function(item) {
-          str += '<li>'+ item.name + '<br/>'+ item.body+ '</li>';
-        }); 
-        
-        str += '</ul>';
-        document.getElementById("slideContainer").innerHTML = str;
-  
-
+    if(localStorage.getItem('browser-schedule')==undefined){
+      localStorage.setItem('browser-schedule', JSON.stringify([]) )
+    }
     document.getElementById('postData').addEventListener('submit', postData);
     function postData(event){
       event.preventDefault();
-      let id=1
+      let id=JSON.parse(localStorage.getItem('browser-schedule')).length
       let name = document.getElementById('name').value;
       let body = document.getElementById('body').value; 
       let time = document.getElementById('time').value;
       let done= false;
-      document.getElementById('postData').addEventListener('submit',function(){
-       
-       
-          if(localStorage.getItem('browser-schedule')!==null){
-         
+                
           value={'id':id, 'name':name, 'body':body, 'time':time, 'done':done};
-          oldSchedule=JSON.parse(localStorage.getItem('browser-schedule'))
-          newSchedule=oldSchedule.concat(value)
-          localStorage.setItem('browser-schedule', JSON.stringify(newSchedule))
-          }else{
-            localStorage.setItem('browser-schedule', JSON.stringify([{'id':id, 'name':name, 'body':body, 'time':time, 'done':done}]) )
-          
-         }        
-      
-     });
+          data=JSON.parse(localStorage.getItem('browser-schedule'))
+          data.push(value);
+          var data2= data
+          localStorage.setItem("browser-schedule", JSON.stringify(data2))
+     
   }
 
-    
 
    document.getElementById('click2').addEventListener('click', function(){
-    chrome.storage.sync.get('schedule', function(items) {
-      console.log(localStorage.getItem('browser-schedule'))
-   })
+   var data=JSON.parse(localStorage.getItem('browser-schedule'))
+        var str = '<ul>'
+
+        data.forEach(function(item) {
+          str += '<li>'+ item.name + '<br/>'+ item.body+ '</li>';
+          if(item.done){
+            str+= '<button>'+'done'+'</button>'
+          }else{
+            str+= '<button>'+'not done'+'</button>'
+          }
+        }); 
+        
+        str += '</ul>';
+        document.getElementById("slideContainer").innerHTML = str;
   });
