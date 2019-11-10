61735ef7a3140eb870cb09aa36d37a4eb77eb6f3
valentine-mario <valentine13400@gmail.com>
2019-11-10 21:20:15 +0100

    added comments and added options to delete all items from storage

--- js/getObject.js
+++ js/getObject.js
@@ -1,3 +1,4 @@
+//function to return route param from the loaded window
 function getParam() {
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
@@ -6,10 +7,13 @@ function getParam() {
     return vars;
 }
 
-
+//passing the param to a param variable
 param=parseInt(getParam()['num']) 
 
+//get the object from localstorage using the param index
 var data=JSON.parse(localStorage.getItem('browser-schedule'))[param]
+
+//pass in object value to HTML
 
 document.getElementById('title').innerHTML=data.name;
 document.getElementById('body').innerHTML=data.body;
@@ -22,25 +26,35 @@ if(data.done==false){
 
 }
 
+//mark item as complete
 document.getElementById('mark').addEventListener('click', function(){
+    //get index of item
     var index= JSON.parse(localStorage.getItem('browser-schedule')).indexOf(data)
       let id=data.id
       let name =data.name;
       let body = data.body; 
       let time = data.time;
       let done= true;
+      //put in object to a new object variable, changing done to true
       value={'id':id, 'name':name, 'body':body, 'time':time, 'done':done};
       data_arr=JSON.parse(localStorage.getItem('browser-schedule'))
+      //delete object from the array
       data_arr.splice(index, 1)
+      //add new modified object back to array at it's previous index
       data_arr.splice(index, 0, value)   
       var data2= data_arr
+      //stringify object and add back to localstorage
       localStorage.setItem("browser-schedule", JSON.stringify(data2))
 })
 
+//delete item from list
 document.getElementById('delete').addEventListener('click', ()=>{
+    //get index of item
     var index= JSON.parse(localStorage.getItem('browser-schedule')).indexOf(data)
     data_arr=JSON.parse(localStorage.getItem('browser-schedule'))
+    //remove item from list
     data_arr.splice(index, 1)
+    //reassign arr to a new variable and save in local storage
     data2= data_arr
     localStorage.setItem("browser-schedule", JSON.stringify(data2))
 
