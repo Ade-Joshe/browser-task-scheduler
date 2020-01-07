05dbadbdafd8c4e2286e514c92624b866872801f
Adegoke Joshua Oluwafemi <19872306+Ade-Joshe@users.noreply.github.com>
2020-01-07 01:43:35 +0100

    Pushed a  modified front end application

--- js/getObject.js
+++ js/getObject.js
@@ -1,87 +1,86 @@
 //function to return route param from the loaded window
 function getParam() {
     var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
         vars[key] = value;
     });
     return vars;
 }
 
 //passing the param to a param variable
-param=parseInt(getParam()['num']) 
+param = parseInt(getParam()['num'])
 
 //get the object from localstorage using the param index
-var arr=JSON.parse(localStorage.getItem('browser-schedule'))
-var data= arr.find(x => x.id === param)
-var data_arr=JSON.parse(localStorage.getItem('browser-schedule'))
+var arr = JSON.parse(localStorage.getItem('browser-schedule'))
+var data = arr.find(x => x.id === param)
+var data_arr = JSON.parse(localStorage.getItem('browser-schedule'))
 var index = data_arr.findIndex(i => i.id === param);
 
 //pass in object value to HTML
 
-document.getElementById('title').innerHTML=data.name;
-document.getElementById('body').innerHTML=data.body;
-document.getElementById('time').innerHTML=data.time;
+document.getElementById('title').innerHTML = data.name;
+document.getElementById('body').innerHTML = data.body;
+document.getElementById('time').innerHTML = data.time;
 
-if(data.done==false){
-    document.getElementById('complete').style.display="none";
-    document.getElementById('unmark').style.display="none"
-}else{
-    document.getElementById('mark').style.display="none"
+if (data.done == false) {
+    document.getElementById('complete').style.display = "none";
+    document.getElementById('unmark').style.display = "none"
+} else {
+    document.getElementById('mark').style.display = "none"
 
 }
 
 //mark item as complete
-document.getElementById('mark').addEventListener('click', function(){
-      let id=data.id
-      let name =data.name;
-      let body = data.body; 
-      let time = data.time;
-      let created_at=data.created_at
-      let done= true;
-      //put in object to a new object variable, changing done to true
-      value={'id':id, 'name':name, 'body':body, 'time':time, "created_at":created_at, 'done':done};
-      
-      //add new modified object back to array at it's previous index
-      data_arr.splice(index, 1)  
-      data_arr.push(value) 
-      var data2= data_arr
-      //stringify object and add back to localstorage
-      localStorage.setItem("browser-schedule", JSON.stringify(data2))
-      document.getElementById('complete').style.display="block";
-      document.getElementById('mark').style.display="none"
-      document.getElementById('unmark').style.display="block"
+document.getElementById('mark').addEventListener('click', function () {
+    let id = data.id
+    let name = data.name;
+    let body = data.body;
+    let time = data.time;
+    let created_at = data.created_at
+    let done = true;
+    //put in object to a new object variable, changing done to true
+    value = { 'id': id, 'name': name, 'body': body, 'time': time, "created_at": created_at, 'done': done };
+
+    //add new modified object back to array at it's previous index
+    data_arr.splice(index, 1)
+    data_arr.push(value)
+    var data2 = data_arr
+    //stringify object and add back to localstorage
+    localStorage.setItem("browser-schedule", JSON.stringify(data2))
+    document.getElementById('complete').style.display = "block";
+    document.getElementById('mark').style.display = "none"
+    document.getElementById('unmark').style.display = "block"
 
 })
 
 //untick item
-document.getElementById('unmark').addEventListener('click', function(){
-      let id=data.id
-      let name =data.name;
-      let body = data.body; 
-      let time = data.time;
-      let created_at=data.created_at
-      let done= false;
-      //put in object to a new object variable, changing done to true
-      value={'id':id, 'name':name, 'body':body, 'time':time, "created_at":created_at, 'done':done};
-      //add new modified object back to array at it's previous index
-      data_arr.splice(index, 1)  
-      data_arr.push(value)  
-      var data2= data_arr
-      //stringify object and add back to localstorage
-      localStorage.setItem("browser-schedule", JSON.stringify(data2))
-      document.getElementById('complete').style.display="none";
-      document.getElementById('mark').style.display="block"
-      document.getElementById('unmark').style.display="none"
+document.getElementById('unmark').addEventListener('click', function () {
+    let id = data.id
+    let name = data.name;
+    let body = data.body;
+    let time = data.time;
+    let created_at = data.created_at
+    let done = false;
+    //put in object to a new object variable, changing done to true
+    value = { 'id': id, 'name': name, 'body': body, 'time': time, "created_at": created_at, 'done': done };
+    //add new modified object back to array at it's previous index
+    data_arr.splice(index, 1)
+    data_arr.push(value)
+    var data2 = data_arr
+    //stringify object and add back to localstorage
+    localStorage.setItem("browser-schedule", JSON.stringify(data2))
+    document.getElementById('complete').style.display = "none";
+    document.getElementById('mark').style.display = "block"
+    document.getElementById('unmark').style.display = "none"
 
 })
 
 //delete item from list
-document.getElementById('delete').addEventListener('click', ()=>{
+document.getElementById('delete').addEventListener('click', () => {
     //remove item from list
     data_arr.splice(index, 1)
     //reassign arr to a new variable and save in local storage
-    data2= data_arr
+    data2 = data_arr
     localStorage.setItem("browser-schedule", JSON.stringify(data2))
     window.location.href = "index.html";
-
 })
--- js/read_data.js
+++ js/read_data.js
@@ -1,59 +1,59 @@
-    //check if browser-schedule local storage is undefined and add empty array just in case user clears their local storage
-    if(localStorage.getItem('browser-schedule')==undefined){
-      localStorage.setItem('browser-schedule', JSON.stringify([]) )
-    }
+//check if browser-schedule local storage is undefined and add empty array just in case user clears their local storage
+if (localStorage.getItem('browser-schedule') == undefined) {
+  localStorage.setItem('browser-schedule', JSON.stringify([]))
+}
 
-    
-    //loop through localstorage item and display content
-   var data=JSON.parse(localStorage.getItem('browser-schedule'))
-        var str = `<div style="padding-left:5px; padding-right:5px;" class="container>`
-          if(data.length < 1){
-            document.getElementById('empty').style.display="block"
-          }else{
-            document.getElementById('empty').style.display="none"
-          }
-        data.forEach(function(item) {
-          
-          str+=`<ul class="list-group">`
-          str+=`<a style="color:black;" href="item.html?num=${JSON.stringify(item.id)}">`
-         str+= `<li class="list-group-item d-flex justify-content-between align-items-center">`+ item.name+
-          `<span class="badge">${item.time}</span>
+
+//loop through localstorage item and display content
+var data = JSON.parse(localStorage.getItem('browser-schedule'))
+var str = `<div style="padding-left:5px; padding-right:5px;" class="container>`
+if (data.length < 1) {
+  document.getElementById('empty').style.display = "block"
+} else {
+  document.getElementById('empty').style.display = "none"
+}
+data.forEach(function (item) {
+
+  str += `<ul class="list-group">`
+  str += `<a class='itemBox' style="color:black; text-decoration:none" href="item.html?num=${JSON.stringify(item.id)}">`
+  str += `<li class="list-group-item d-flex justify-content-between align-items-center">` + item.name +
+    `<span class="badge">${item.time}</span>
           </li>`
-          str+=`</a>`
-          if(item.done){
-            str+= `<p style="font-size:20px;">`+'&#x2713;'+'</p>'
-          }else{
-            str+= '<p>'+'pending'+'</p>'
-          }
-          
-        }); 
-          str+=`</ul>`
-          str += '</div>';
-          document.getElementById("slideContainer").innerHTML = str;
-          
-  
+  str += `</a>`
+  if (item.done) {
+    str += `<p style="font-size:10px; color: green">` + '&#x2713; Done' + '</p>'
+  } else {
+    str += '<p style="font-size: 10px; color: tomato">' + 'pending' + '</p>'
+  }
+
+});
+str += `</ul>`
+str += '</div>';
+document.getElementById("slideContainer").innerHTML = str;
+
+
 //delete all items in local storage
-document.getElementById('delete').addEventListener('click', ()=>{
-  localStorage.setItem('browser-schedule', JSON.stringify([]) )
-  document.getElementById('slideContainer').innerHTML="";
-  document.getElementById('empty').style.display="block"
+document.getElementById('delete').addEventListener('click', () => {
+  localStorage.setItem('browser-schedule', JSON.stringify([]))
+  document.getElementById('slideContainer').innerHTML = "";
+  document.getElementById('empty').style.display = "block"
 
 })
 
 
 //check if item has been in localstorage for over or equal 24 hours
 //if true delete item
-var data=JSON.parse(localStorage.getItem('browser-schedule'))
-data.forEach((item)=>{
-  var time_now={time: Date.now()}
-  diff= Math.abs(item.created_at - time_now.time) / 36e5;
-  if(diff>=24){
-    var index= JSON.parse(localStorage.getItem('browser-schedule')).indexOf(item)
-    data_arr=JSON.parse(localStorage.getItem('browser-schedule'))
+var data = JSON.parse(localStorage.getItem('browser-schedule'))
+data.forEach((item) => {
+  var time_now = { time: Date.now() }
+  diff = Math.abs(item.created_at - time_now.time) / 36e5;
+  if (diff >= 24) {
+    var index = JSON.parse(localStorage.getItem('browser-schedule')).indexOf(item)
+    data_arr = JSON.parse(localStorage.getItem('browser-schedule'))
     //remove item from list
     data_arr.splice(index, 1)
     //reassign arr to a new variable and save in local storage
-    data2= data_arr
+    data2 = data_arr
     localStorage.setItem("browser-schedule", JSON.stringify(data2))
   }
 })
